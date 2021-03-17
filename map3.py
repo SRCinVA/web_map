5a84829ce7c441e31813a1349c8366cf4cab142a
SRCinVA <scitarella@hotmail.com>
2021-03-16 20:53:24 -0400

    multiple markers today

--- map3.py
+++ map3.py
@@ -1,6 +1,13 @@
 import folium
 map = folium.Map(location=[33.99,-77.5], zoom_start=6, tiles = "Stamen Terrain")
-# we can add objects to that map
-map.add_child(folium.Marker(location = [33.98,-77.6], popup= "Greetings! I'm a marker.", icon=folium.Icon(color ="green")))
+
+# another way to add children is a feature group
+fg = folium.FeatureGroup(name="My Map")
+
+# we can add objects to that map. Multiple markers require multiple methods, or you can use a for loop:
+for coordinates in [[33.98, -77.6], [35.00, -77.6]]:
+    map.add_child(folium.Marker(location = coordinates, popup = "Greetings! I'm a marker.", icon=folium.Icon(color ="green")))
+
+map.add_child(fg) # this helps you keep your code organized when you want to add other layers
 
 map.save("Map3.html")
