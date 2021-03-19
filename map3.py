14fa32e13c450d65ea171a248e66b3d6f85aa751
SRCinVA <scitarella@hotmail.com>
2021-03-18 21:31:25 -0400

    added elevation to markers

--- map3.py
+++ map3.py
@@ -5,6 +5,7 @@ data = pandas.read_csv("Volcanoes.txt")
 
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[33.99,-77.5], zoom_start=6, tiles = "Stamen Terrain")
 
@@ -15,9 +16,9 @@ fg = folium.FeatureGroup(name="My Map")
 # for coordinates in [[33.98, -77.6], [35.00, -77.6]]:
 #   map.add_child(folium.Marker(location = coordinates, popup = "Greetings! I'm a marker.", icon=folium.Icon(color ="green")))
 
-# the zip function lets you toggle/pair items from 2 separate lists
-for lt,ln in zip(lat,lon):
-    map.add_child(folium.Marker(location = [lt, ln], popup = "Greetings! I'm a marker.", icon=folium.Icon(color ="green")))
+# the zip function lets you iterate over items from multiple separate lists
+for lt,ln,el in zip(lat,lon,elev): # strangely, never got his type error; folium had trouble with floats for my code.
+    map.add_child(folium.Marker(location = [lt, ln], popup = str(el) + " m", icon=folium.Icon(color ="green")))
 
 
 map.add_child(fg) # this helps you keep your code organized when you want to add other layers
