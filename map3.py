a01bd1e6e407094b517d403081e14cfea7c697e1
SRCinVA <scitarella@hotmail.com>
2021-03-19 23:01:25 -0400

    added colors to markers

--- map3.py
+++ map3.py
@@ -7,6 +7,25 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+# you need to create the function before you need it in the for loop below
+def color_producer(elev):
+    if elev <= 1000: # you cast them as ints but pass them as strings -- what the ...?
+        return "green"
+    if elev >= 1001 and elev <= 2000:
+        return "orange"
+    if elev >= 2001:
+        return "red"
+
+# Ardit's code
+#  if elevation < 1000:
+#      return 'green'
+#  elif 1000 <= elevation < 3000:
+#      return 'orange'
+#  else:
+#      retturn 'red'
+
+
+
 map = folium.Map(location=[33.99,-77.5], zoom_start=6, tiles = "Stamen Terrain")
 
 # another way to add children is a feature group
@@ -18,7 +37,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 # the zip function lets you iterate over items from multiple separate lists
 for lt,ln,el in zip(lat,lon,elev): # strangely, never got his type error; folium had trouble with floats for my code.
-    map.add_child(folium.Marker(location = [lt, ln], popup = str(el) + " m", icon=folium.Icon(color ="green")))
+    map.add_child(folium.Marker(location = [lt, ln], popup = str(el) + " m", icon=folium.Icon(color = color_producer(el)))) # need to pass <el> to color_producer()
 
 
 map.add_child(fg) # this helps you keep your code organized when you want to add other layers
